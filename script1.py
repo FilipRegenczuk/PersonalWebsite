f9a3d92852196144e4bdb7b0ba742ec59a826d7d
FilipRegenczuk <f.regenczuk@outlook.com>
2019-10-29 06:56:04 +0100

    connection test

--- script1.py
+++ script1.py
@@ -1,1 +1,10 @@
-#this is a test
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Content of website"
+
+if __name__ == "__main__":
+    app.run(debug=True)
